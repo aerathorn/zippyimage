ddd473deecc04da86e9b2ccad94b1f5507792341
aerathorn <jblopez.66@gmail.com>
2025-07-03 22:48:14 +0800

    Added postbuild script for sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+// next-sitemap.js
+module.exports = {
+  siteUrl: 'https://zippyimage.vercel.app', // Your live site URL
+  generateRobotsTxt: true, // Optional: Generate robots.txt file
+  changefreq: 'daily', // Change frequency
+  priority: 0.7, // Default priority
+}
